271521e9e23a419b9c64228aeca4b6a638a9b81c
Abhiram-paturi <abhirampaturi0100@gmail.com>
2026-04-09 16:06:39 +0000

    new file:   .vscode/settings.json 	new file:   api-tests.http 	modified:   inventory-service/src/main/java/com/example/inventoryservice/InventoryserviceApplication.java 	new file:   inventory-service/src/main/java/com/example/inventoryservice/controller/InventoryController.java 	new file:   inventory-service/src/main/java/com/example/inventoryservice/dto/InventoryResponse.java 	new file:   inventory-service/src/main/java/com/example/inventoryservice/model/Inventory.java 	new file:   inventory-service/src/main/java/com/example/inventoryservice/repository/InventoryRepository.java 	new file:   inventory-service/src/main/java/com/example/inventoryservice/service/InventoryService.java 	modified:   inventory-service/src/main/resources/application.properties 	modified:   orderserivce/pom.xml 	new file:   orderserivce/src/main/java/com/example/orderservice/DTO/InventoryResponse.java 	new file:   orderserivce/src/main/java/com/example/orderservice/config/WebClientConfig.java 	modified:   orderserivce/src/main/java/com/example/orderservice/service/OrderService.java 	modified:   orderserivce/src/main/resources/application.properties

--- inventory-service/src/main/java/com/example/inventoryservice/InventoryserviceApplication.java
+++ inventory-service/src/main/java/com/example/inventoryservice/InventoryserviceApplication.java
@@ -1,7 +1,12 @@
 package com.example.inventoryservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.inventoryservice.model.Inventory;
+import com.example.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
 public class InventoryserviceApplication {
@@ -10,4 +15,19 @@ public class InventoryserviceApplication {
 		SpringApplication.run(InventoryserviceApplication.class, args);
 	}
 
+     @Bean
+	 public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+           Inventory inventory1 = new Inventory();
+		   inventory1.setSkuCode("iphone_13");
+		   inventory1.setQuantity(200);
+           
+		   Inventory inventory2 = new Inventory();
+		   inventory2.setSkuCode("iphone_13_pro");
+		   inventory2.setQuantity(0);
+
+		   inventoryRepository.save(inventory1);
+		   inventoryRepository.save(inventory2);
+		};
+	 }
 }
--- orderserivce/src/main/java/com/example/orderservice/service/OrderService.java
+++ orderserivce/src/main/java/com/example/orderservice/service/OrderService.java
@@ -1,11 +1,14 @@
 package com.example.orderservice.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.example.orderservice.DTO.InventoryResponse;
 import com.example.orderservice.DTO.OrderLineItemsDto;
 import com.example.orderservice.DTO.OrderRequest;
 import com.example.orderservice.model.Order;
@@ -20,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -32,7 +36,25 @@ public class OrderService {
 
         order.setOrderLineItems(orderLineiItems );
 
+      List<String> skuCodes= order.getOrderLineItems().stream()
+                                 .map(orderLineiItem -> orderLineiItem.getSkuCode())
+                                 .toList();
+
+        //call Inventory Service , and place order if product 
+        //is in stock
+      InventoryResponse[] inventoryResponseArray = webClient.get()
+                                  .uri("http://localhost:8082/api/inventory",
+                                   uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                                  .retrieve()
+                                  .bodyToMono(InventoryResponse[].class)
+                                  .block();
+       boolean allProductsInStock =  Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse -> InventoryResponse.getIsInStock());
+  
+        if(allProductsInStock){
         orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is not in stock, please try agian later ");
+        }
     }
 
     private OrderLineiItems mapToDto(OrderLineItemsDto orderLineItemsDto){
